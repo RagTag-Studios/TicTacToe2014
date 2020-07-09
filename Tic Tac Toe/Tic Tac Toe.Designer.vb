3a43285012b637acbf42a7cd8af5ac9776a67a2c
trekmania <trekmania@yahoo.com>
2020-07-09 18:43:00 -0400

    initial commit... i guess?

---
+++ Tic Tac Toe/Tic Tac Toe.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.square6 = New System.Windows.Forms.Button()
+        Me.square1 = New System.Windows.Forms.Button()
+        Me.square7 = New System.Windows.Forms.Button()
+        Me.square9 = New System.Windows.Forms.Button()
+        Me.square8 = New System.Windows.Forms.Button()
+        Me.square2 = New System.Windows.Forms.Button()
+        Me.square3 = New System.Windows.Forms.Button()
+        Me.square5 = New System.Windows.Forms.Button()
+        Me.square4 = New System.Windows.Forms.Button()
+        Me.Player1Label = New System.Windows.Forms.Label()
+        Me.Player2Label = New System.Windows.Forms.Label()
+        Me.drawLabel = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'square6
+        '
+        Me.square6.BackColor = System.Drawing.Color.White
+        Me.square6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.square6.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.square6.FlatAppearance.BorderSize = 0
+        Me.square6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.square6.Location = New System.Drawing.Point(490, 229)
+        Me.square6.Name = "square6"
+        Me.square6.Size = New System.Drawing.Size(138, 129)
+        Me.square6.TabIndex = 9
+        Me.square6.UseVisualStyleBackColor = False
+        '
+        'square1
+        '
+        Me.square1.BackColor = System.Drawing.Color.White
+        Me.square1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.square1.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.square1.FlatAppearance.BorderSize = 0
+        Me.square1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.square1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.square1.Location = New System.Drawing.Point(168, 80)
+        Me.square1.Name = "square1"
+        Me.square1.Size = New System.Drawing.Size(138, 129)
+        Me.square1.TabIndex = 9
+        Me.square1.UseVisualStyleBackColor = False
+        '
+        'square7
+        '
+        Me.square7.BackColor = System.Drawing.Color.White
+        Me.square7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.square7.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.square7.FlatAppearance.BorderSize = 0
+        Me.square7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.square7.Location = New System.Drawing.Point(168, 377)
+        Me.square7.Name = "square7"
+        Me.square7.Size = New System.Drawing.Size(138, 129)
+        Me.square7.TabIndex = 9
+        Me.square7.UseVisualStyleBackColor = False
+        '
+        'square9
+        '
+        Me.square9.BackColor = System.Drawing.Color.White
+        Me.square9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.square9.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.square9.FlatAppearance.BorderSize = 0
+        Me.square9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.square9.Location = New System.Drawing.Point(490, 377)
+        Me.square9.Name = "square9"
+        Me.square9.Size = New System.Drawing.Size(138, 129)
+        Me.square9.TabIndex = 9
+        Me.square9.UseVisualStyleBackColor = False
+        '
+        'square8
+        '
+        Me.square8.BackColor = System.Drawing.Color.White
+        Me.square8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.square8.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.square8.FlatAppearance.BorderSize = 0
+        Me.square8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.square8.Location = New System.Drawing.Point(329, 377)
+        Me.square8.Name = "square8"
+        Me.square8.Size = New System.Drawing.Size(138, 129)
+        Me.square8.TabIndex = 9
+        Me.square8.UseVisualStyleBackColor = False
+        '
+        'square2
+        '
+        Me.square2.BackColor = System.Drawing.Color.White
+        Me.square2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.square2.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.square2.FlatAppearance.BorderSize = 0
+        Me.square2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.square2.Location = New System.Drawing.Point(329, 80)
+        Me.square2.Name = "square2"
+        Me.square2.Size = New System.Drawing.Size(138, 129)
+        Me.square2.TabIndex = 9
+        Me.square2.UseVisualStyleBackColor = False
+        '
+        'square3
+        '
+        Me.square3.BackColor = System.Drawing.Color.White
+        Me.square3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.square3.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.square3.FlatAppearance.BorderSize = 0
+        Me.square3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.square3.Location = New System.Drawing.Point(490, 80)
+        Me.square3.Name = "square3"
+        Me.square3.Size = New System.Drawing.Size(138, 129)
+        Me.square3.TabIndex = 9
+        Me.square3.UseVisualStyleBackColor = False
+        '
+        'square5
+        '
+        Me.square5.BackColor = System.Drawing.Color.White
+        Me.square5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.square5.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.square5.FlatAppearance.BorderSize = 0
+        Me.square5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.square5.Location = New System.Drawing.Point(329, 229)
+        Me.square5.Name = "square5"
+        Me.square5.Size = New System.Drawing.Size(138, 129)
+        Me.square5.TabIndex = 9
+        Me.square5.UseVisualStyleBackColor = False
+        '
+        'square4
+        '
+        Me.square4.BackColor = System.Drawing.Color.White
+        Me.square4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.square4.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.square4.FlatAppearance.BorderSize = 0
+        Me.square4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.square4.Location = New System.Drawing.Point(168, 229)
+        Me.square4.Name = "square4"
+        Me.square4.Size = New System.Drawing.Size(138, 129)
+        Me.square4.TabIndex = 10
+        Me.square4.UseVisualStyleBackColor = False
+        '
+        'Player1Label
+        '
+        Me.Player1Label.AutoSize = True
+        Me.Player1Label.Font = New System.Drawing.Font("Verdana", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Player1Label.ForeColor = System.Drawing.Color.Red
+        Me.Player1Label.Location = New System.Drawing.Point(24, 9)
+        Me.Player1Label.Name = "Player1Label"
+        Me.Player1Label.Size = New System.Drawing.Size(156, 23)
+        Me.Player1Label.TabIndex = 11
+        Me.Player1Label.Text = "Player 1 Turn"
+        '
+        'Player2Label
+        '
+        Me.Player2Label.AutoSize = True
+        Me.Player2Label.Font = New System.Drawing.Font("Verdana", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Player2Label.ForeColor = System.Drawing.Color.Blue
+        Me.Player2Label.Location = New System.Drawing.Point(617, 9)
+        Me.Player2Label.Name = "Player2Label"
+        Me.Player2Label.Size = New System.Drawing.Size(156, 23)
+        Me.Player2Label.TabIndex = 12
+        Me.Player2Label.Text = "Player 2 Turn"
+        Me.Player2Label.Visible = False
+        '
+        'drawLabel
+        '
+        Me.drawLabel.AutoSize = True
+        Me.drawLabel.Font = New System.Drawing.Font("Verdana", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.drawLabel.ForeColor = System.Drawing.Color.Black
+        Me.drawLabel.Location = New System.Drawing.Point(362, 9)
+        Me.drawLabel.Name = "drawLabel"
+        Me.drawLabel.Size = New System.Drawing.Size(77, 23)
+        Me.drawLabel.TabIndex = 13
+        Me.drawLabel.Text = "DRAW"
+        Me.drawLabel.Visible = False
+        '
+        'btnReset
+        '
+        Me.btnReset.Font = New System.Drawing.Font("Verdana", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.Location = New System.Drawing.Point(50, 570)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(130, 74)
+        Me.btnReset.TabIndex = 14
+        Me.btnReset.Text = "Reset Game"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Font = New System.Drawing.Font("Verdana", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnQuit.Location = New System.Drawing.Point(621, 570)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(130, 74)
+        Me.btnQuit.TabIndex = 14
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(803, 656)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.drawLabel)
+        Me.Controls.Add(Me.Player2Label)
+        Me.Controls.Add(Me.Player1Label)
+        Me.Controls.Add(Me.square4)
+        Me.Controls.Add(Me.square5)
+        Me.Controls.Add(Me.square8)
+        Me.Controls.Add(Me.square9)
+        Me.Controls.Add(Me.square7)
+        Me.Controls.Add(Me.square3)
+        Me.Controls.Add(Me.square2)
+        Me.Controls.Add(Me.square1)
+        Me.Controls.Add(Me.square6)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "Form1"
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents square6 As System.Windows.Forms.Button
+    Friend WithEvents square1 As System.Windows.Forms.Button
+    Friend WithEvents square7 As System.Windows.Forms.Button
+    Friend WithEvents square9 As System.Windows.Forms.Button
+    Friend WithEvents square8 As System.Windows.Forms.Button
+
+    Friend WithEvents square2 As System.Windows.Forms.Button
+    Friend WithEvents square3 As System.Windows.Forms.Button
+    Friend WithEvents square5 As System.Windows.Forms.Button
+    Friend WithEvents square4 As System.Windows.Forms.Button
+    Friend WithEvents Player1Label As System.Windows.Forms.Label
+    Friend WithEvents Player2Label As System.Windows.Forms.Label
+    Friend WithEvents drawLabel As System.Windows.Forms.Label
+    Friend WithEvents btnReset As System.Windows.Forms.Button
+    Friend WithEvents btnQuit As System.Windows.Forms.Button
+
+End Class
---
+++ Tic Tac Toe/Tic Tac Toe.vb
@@ -0,0 +1,333 @@
+﻿Public Class Form1
+
+    'define global variables
+    Dim playerTurn As String = "Player1"
+    Dim numberOfTurns As Integer = 0
+
+    Private Sub square1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles square1.Click
+        square1.Enabled = False
+        If playerTurn = "Player1" Then
+            square1.BackColor = Color.Red
+            player1Turn()
+        ElseIf playerTurn = "Player2" Then
+            square1.BackColor = Color.Blue
+            player2Turn()
+        End If
+
+    End Sub
+
+    Private Sub square2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles square2.Click
+        square2.Enabled = False
+        If playerTurn = "Player1" Then
+            square2.BackColor = Color.Red
+            player1Turn()
+        ElseIf playerTurn = "Player2" Then
+            square2.BackColor = Color.Blue
+            player2Turn()
+        End If
+
+    End Sub
+
+    Private Sub square3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles square3.Click
+        square3.Enabled = False
+        If playerTurn = "Player1" Then
+            square3.BackColor = Color.Red
+            player1Turn()
+        ElseIf playerTurn = "Player2" Then
+            square3.BackColor = Color.Blue
+            player2Turn()
+        End If
+
+    End Sub
+
+    Private Sub square4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles square4.Click
+        square4.Enabled = False
+        If playerTurn = "Player1" Then
+            square4.BackColor = Color.Red
+            player1Turn()
+        ElseIf playerTurn = "Player2" Then
+            square4.BackColor = Color.Blue
+            player2Turn()
+        End If
+
+    End Sub
+
+    Private Sub square5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles square5.Click
+        square5.Enabled = False
+        If playerTurn = "Player1" Then
+            square5.BackColor = Color.Red
+            player1Turn()
+        ElseIf playerTurn = "Player2" Then
+            square5.BackColor = Color.Blue
+            player2Turn()
+        End If
+
+    End Sub
+
+    Private Sub square6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles square6.Click
+        square6.Enabled = False
+        If playerTurn = "Player1" Then
+            square6.BackColor = Color.Red
+            player1Turn()
+        ElseIf playerTurn = "Player2" Then
+            square6.BackColor = Color.Blue
+            player2Turn()
+        End If
+
+    End Sub
+
+    Private Sub square7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles square7.Click
+        square7.Enabled = False
+        If playerTurn = "Player1" Then
+            square7.BackColor = Color.Red
+            player1Turn()
+        ElseIf playerTurn = "Player2" Then
+            square7.BackColor = Color.Blue
+            player2Turn()
+        End If
+
+    End Sub
+
+    Private Sub square8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles square8.Click
+        square8.Enabled = False
+        If playerTurn = "Player1" Then
+            square8.BackColor = Color.Red
+            player1Turn()
+        ElseIf playerTurn = "Player2" Then
+            square8.BackColor = Color.Blue
+            player2Turn()
+        End If
+
+    End Sub
+
+
+    Private Sub square9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles square9.Click
+        square9.Enabled = False
+        If playerTurn = "Player1" Then
+            square9.BackColor = Color.Red
+            player1Turn()
+        ElseIf playerTurn = "Player2" Then
+            square9.BackColor = Color.Blue
+            player2Turn()
+        End If
+
+    End Sub
+
+    Private Function isGameOver() As Boolean
+        'There are 8 ways to get three in a row in Tic Tac Toe:
+        '3 in a row in any row
+        '3 in a row in any column
+        '3 in a row diagonally
+        'if all squares have been selected, yet there is no three in a row, then the game is a draw
+
+        'Scenario 1: if the first row has three in a row (squares 1, 2, 3)
+        If square1.BackColor = Color.Red And square2.BackColor = Color.Red And square3.BackColor = Color.Red Then
+            player1Wins()
+            Return True
+        ElseIf square1.BackColor = Color.Blue And square2.BackColor = Color.Blue And square3.BackColor = Color.Blue Then
+            player2Wins()
+            Return True
+        End If
+
+        'Scenario 2: if the second row has three in a row (squares 4, 5, 6)
+        If square4.BackColor = Color.Red And square5.BackColor = Color.Red And square6.BackColor = Color.Red Then
+            player1Wins()
+            Return True
+        ElseIf square4.BackColor = Color.Blue And square5.BackColor = Color.Blue And square6.BackColor = Color.Blue Then
+            player2Wins()
+            Return True
+        End If
+
+        'Scenario 3: if third row has three in a row (squares 7, 8, 9)
+        If square7.BackColor = Color.Red And square8.BackColor = Color.Red And square9.BackColor = Color.Red Then
+            player1Wins()
+            Return True
+        ElseIf square7.BackColor = Color.Blue And square8.BackColor = Color.Blue And square9.BackColor = Color.Blue Then
+            player2Wins()
+            Return True
+        End If
+
+        'Scenario 4: if the first column has three in a row (squares 1, 4, 7)
+        If square1.BackColor = Color.Red And square4.BackColor = Color.Red And square7.BackColor = Color.Red Then
+            player1Wins()
+            Return True
+        ElseIf square1.BackColor = Color.Blue And square4.BackColor = Color.Blue And square7.BackColor = Color.Blue Then
+            player2Wins()
+            Return True
+        End If
+
+        'Scenario 5: if the second column has three in a row (squares 2, 5, 8)
+        If square2.BackColor = Color.Red And square5.BackColor = Color.Red And square8.BackColor = Color.Red Then
+            player1Wins()
+            Return True
+        ElseIf square2.BackColor = Color.Blue And square5.BackColor = Color.Blue And square8.BackColor = Color.Blue Then
+            player2Wins()
+            Return True
+        End If
+
+        'Scenario 6: if the third column has three in a row (squares 3, 6, 9)
+        If square3.BackColor = Color.Red And square6.BackColor = Color.Red And square9.BackColor = Color.Red Then
+            player1Wins()
+            Return True
+        ElseIf square3.BackColor = Color.Blue And square6.BackColor = Color.Blue And square9.BackColor = Color.Blue Then
+            player2Wins()
+            Return True
+        End If
+
+        'Scenario 7: if diagonally three in a row (squares 1, 5, 9)
+        If square1.BackColor = Color.Red And square5.BackColor = Color.Red And square9.BackColor = Color.Red Then
+            player1Wins()
+            Return True
+        ElseIf square1.BackColor = Color.Blue And square5.BackColor = Color.Blue And square9.BackColor = Color.Blue Then
+            player2Wins()
+            Return True
+        End If
+
+        'Scenario 8: if diagonally three in a row (squares 7, 5 ,3)
+        If square7.BackColor = Color.Red And square5.BackColor = Color.Red And square3.BackColor = Color.Red Then
+            player1Wins()
+            Return True
+        ElseIf square7.BackColor = Color.Blue And square5.BackColor = Color.Blue And square3.BackColor = Color.Blue Then
+            player2Wins()
+            Return True
+        End If
+
+        'Draw scenario: if all squares have been seleccted, yet no winner then it is a draw
+        If numberOfTurns = 9 Then
+            gameIsDraw()
+            Return True
+        End If
+
+
+        Return False
+
+    End Function
+
+    Private Sub player1Turn()
+        'call this code at game start and every time
+        'player 1 takes a turn
+
+        'switch the labels
+        Player1Label.Visible = False
+        Player2Label.Visible = True
+
+        'transfer control to player 2
+        playerTurn = "Player2"
+
+        'increment turn count
+        numberOfTurns += 1
+
+        'see if game over or winner
+        isGameOver()
+
+    End Sub
+    Private Sub player2Turn()
+        'call this code every time player 2 takes a turn
+
+        'switch the labels
+        Player2Label.Visible = False
+        Player1Label.Visible = True
+
+        'transfer control to player 1
+        playerTurn = "Player1"
+
+        'increment turn count
+        numberOfTurns += 1
+
+        'see if game over or winner
+        isGameOver()
+
+    End Sub
+    Private Sub player1Wins()
+        disableButtons()
+        MessageBox.Show("Player 1 wins!")
+        Player1Label.Visible = True
+        Player2Label.Visible = False
+        Player1Label.Text = "Winner!"
+        btnReset.Text = "Play Again"
+    End Sub
+
+    Private Sub player2Wins()
+        disableButtons()
+        MessageBox.Show("Player 2 wins!")
+        Player2Label.Visible = True
+        Player1Label.Visible = False
+        Player2Label.Text = "Winner!"
+        btnReset.Text = "Play Again"
+    End Sub
+
+    Private Sub gameIsDraw()
+        MessageBox.Show("Game is a draw!")
+        Player1Label.Visible = False
+        Player2Label.Visible = False
+        drawLabel.Visible = True
+        btnReset.Text = "Play Again"
+    End Sub
+
+    Private Sub btnQuit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuit.Click
+        'quit the game and close the form
+        Me.Close()
+    End Sub
+
+    Private Sub btnReset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnReset.Click
+        'reset all aspects of the game
+
+        'reset all the squares to original state and enable them
+        square1.BackColor = Color.White
+        square1.Enabled = True
+
+        square2.BackColor = Color.White
+        square2.Enabled = True
+
+        square3.BackColor = Color.White
+        square3.Enabled = True
+
+        square4.BackColor = Color.White
+        square4.Enabled = True
+
+        square5.BackColor = Color.White
+        square5.Enabled = True
+
+        square6.BackColor = Color.White
+        square6.Enabled = True
+
+        square7.BackColor = Color.White
+        square7.Enabled = True
+
+        square8.BackColor = Color.White
+        square8.Enabled = True
+
+        square9.BackColor = Color.White
+        square9.Enabled = True
+
+        'reset the turn back to player 1 and reset labels
+        playerTurn = "Player1"
+        Player1Label.Text = "Player 1"
+        Player2Label.Text = "Player 2"
+        Player1Label.Visible = True
+        Player2Label.Visible = False
+        drawLabel.Visible = False
+
+        'reset turn count
+        numberOfTurns = 0
+
+
+        'change reset button text
+        btnReset.Text = "Reset Game"
+
+    End Sub
+
+    Private Sub disableButtons()
+        'Disables all the squares to end game.  
+        'Forces user to reset game.
+        square1.Enabled = False
+        square2.Enabled = False
+        square3.Enabled = False
+        square4.Enabled = False
+        square5.Enabled = False
+        square6.Enabled = False
+        square7.Enabled = False
+        square8.Enabled = False
+        square9.Enabled = False
+    End Sub
+End Class
